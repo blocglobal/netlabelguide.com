0451a94365a5b583cce1e2240cb0f32b4526a1e4
Mike Gregoire <mgregoire@nvzion.com>
2020-08-19 17:34:26 -0400

    Removed bandcamp parsing, requests get blocked

--- utils/parseReleases.js
+++ utils/parseReleases.js
@@ -1,8 +1,4 @@
 import Parser from 'rss-parser';
-import axios from 'axios';
-import he from 'he';
-import dayjs from 'dayjs';
-import cheerio from 'cheerio';
 import urlKeys from './urlKeys';
 
 const parseReleases = async netlabel => {
@@ -10,57 +6,10 @@ const parseReleases = async netlabel => {
 
   let releases = null;
 
-  if (netlabel.urls && netlabel.urls[urlKeys.RELEASES]) {
-    const feed = await parser.parseURL(netlabel.urls[urlKeys.RELEASES]);
-    releases = feed.items;
-  }
-
-  if (releases === null && netlabel.urls && netlabel.urls[urlKeys.BANDCAMP]) {
-    const url = netlabel.urls[urlKeys.BANDCAMP];
-    const res = await axios.get(url);
-    let $ = cheerio.load(res.data);
-
-    releases = [];
-    let promises = [];
-
-    $('li.music-grid-item').each((i, el) => {
-      const item = $(el).find('a');
-      const href = `${url.substring(0, url.length - 1)}${$(item).attr('href')}`;
-      const titleAndArtist = $(item).find('.title').html().split('<br>');
-      let title = titleAndArtist[0].replace(/\n/g, '').trim();
-
-      let artist;
-
-      if (titleAndArtist.length > 1) {
-        artist = cheerio.load(titleAndArtist[1]).text().trim();
-      } else {
-        artist = netlabel.label_name;
-      }
-
-      promises.push(axios.get(href));
-
-      releases.push({
-        title: he.unescape(`${artist} — ${title}`),
-        link: href,
-      });
-    });
-
-    if (promises.length > 0) {
-      await Promise.all(promises).then(responses => {
-        responses.map(response => {
-          releases.map(release => {
-            if (release.link === response.config.url) {
-              let $ = cheerio.load(response.data);
-              const pubDate = $('meta[itemprop="datePublished"]').attr(
-                'content'
-              );
-              release.pubDate = dayjs(pubDate).format(
-                `ddd, DD MMM YYYY 00:00:00 +0000`
-              );
-            }
-          });
-        });
-      });
+  if (netlabel.activity_state === 'active') {
+    if (netlabel.urls && netlabel.urls[urlKeys.RELEASES]) {
+      const feed = await parser.parseURL(netlabel.urls[urlKeys.RELEASES]);
+      releases = feed.items;
     }
   }
 

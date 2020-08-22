aeb630e9ec9cf7d583d61e759080e26429e0ead3
Mike Gregoire <mgregoire@nvzion.com>
2020-08-21 21:55:59 -0400

    Add github link to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import IconLink from './IconLink';
 import './Footer.scss';
 
 const Footer = () => {
@@ -7,6 +10,15 @@ const Footer = () => {
       &copy; {new Date().getFullYear()}, netlabelguide.com
       <br />
       Created by <a href="https://blocsonic.com">blocGLOBAL / blocSonic.com</a>
+      <p>
+        <a
+          href="https://github.com/blocglobal/netlabelguide.com"
+          aria-label="Official GitHub repository"
+          className="github"
+        >
+          <FontAwesomeIcon icon={faGithub} className="icon" />
+        </a>
+      </p>
     </footer>
   );
 };

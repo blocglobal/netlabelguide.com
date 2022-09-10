7da733e662a82827be5c6c947be611ff96dad4a5
Mike Gregoire <mgregoire@nvzion.com>
2022-09-09 20:08:19 -0400

    September 2022 Update

--- pages/index.js
+++ pages/index.js
@@ -19,36 +19,8 @@ const HomePage = () => {
       <div className="hero">
         <p>
           <strong>Update:</strong> Added{' '}
-          <Link href="/netlabel/dr-noisem-tapes">
-            <a>Dr. NoiseM Tapes</a>
-          </Link>
-          ,{' '}
-          <Link href="/netlabel/dust-stream-records">
-            <a>Dust Stream Records</a>
-          </Link>
-          ,{' '}
-          <Link href="/netlabel/eyes-wide-open-records">
-            <a>Eyes Wide Open Records</a>
-          </Link>
-          ,{' '}
-          <Link href="/netlabel/fresscode-records">
-            <a>Fresscode Records</a>
-          </Link>
-          ,{' '}
-          <Link href="/netlabel/nuke-the-planet">
-            <a>Nuke The Planet</a>
-          </Link>
-          ,{' '}
-          <Link href="/netlabel/psychocandies">
-            <a>Psychocandies</a>
-          </Link>
-          ,{' '}
-          <Link href="/netlabel/xdd-x-static-dance-departement">
-            <a>X.D.D. X-static Dance Departement</a>
-          </Link>{' '}
-          and{' '}
-          <Link href="">
-            <a>Zarkon Records</a>
+          <Link href="/netlabel/fischfresse">
+            <a>Fischfresse</a>
           </Link>
         </p>
       </div>
